068f2f9f1725f27a47c80536afded08bb3dd1049
Felix Von nagel <fvon-nag@c2s16d91.42wolfsburg.de>
2023-03-07 15:13:35 +0100

    fixed sort5

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 08:33:02 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/03/07 12:40:56 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/03/07 14:55:40 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 // add error checks (non integers,etc)
+// sort small does not work for 4 67 3 87 23
 int	createstacks(int argc, char **argv, t_stacks *s)
 {
 	int	i;
--- sortsmall.c
+++ sortsmall.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:47:29 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/03/07 09:02:26 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/03/07 15:13:01 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,18 @@ void	sort5(t_stacks *s)
 	sort3(s);
 	if (s->b[0] < s->b[1])
 		sb(s);
-
 	while (s->bs > 0)
 	{
-		if (findind(s->b[0], s) == findind(s->a[2], s) + 1
+		if (findind(s->b[0], s) == findind(s->a[s->as - 1], s) + 1
 			|| findind(s->b[0], s) == findind(s->a[0], s) - 1)
 			pa(s);
+		else if (findind(s->b[0], s) == findind(s->a[s->as -1], s) - 1)
+			rra(s);
 		else
 			ra(s);
 	}
-	while (s->a[0] > s->a[s->as -1])
+	while (s->a[0] > s->a[s->as -1] || s->a[0] > s->a[1] || s->a[0] > s->a[2]
+		|| s->a[0] > s->a[3])
 		rra(s);
 
 }
