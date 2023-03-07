31b138d4d98ada49f2595047baf3b1977d0a02b0
Felix <vboxuser@Coding.myguest.virtualbox.org>
2023-03-07 17:25:43 +0100

    fixed sortsmall

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 08:33:02 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/03/07 16:56:04 by fvon-nag         ###   ########.fr       */
+/*   Updated: 2023/03/07 17:10:05 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,6 @@ int	main(int argc, char **argv)
 	createstacks(argc, argv, &s);
 	sortbysize(&s);
 
-	//printarrays(s);
+	printarrays(s);
 	return (0);
 }
